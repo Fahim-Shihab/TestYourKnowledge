8c3e97b031cd0f6cd67ac30decc1ef3c2af279c1
Sourav Deb <33459970+debsourav33@users.noreply.github.com>
2018-11-10 01:55:47 +0600

    Add files via upload

--- src/debs/Predict.java
+++ src/debs/Predict.java
@@ -17,29 +17,37 @@ import javafx.stage.Stage;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Predict extends Application {
+    public static Stage classStage;
     ArrayList<Code> Codes= new ArrayList<>();
-    Text txtQuestion;
+    Text txtQuestion, txtScore;
     TextArea txtCode;
     TextField txtAns;
     Button btnSubmit;
+    int pos= 0, corr=0;
 
     private void init(GridPane gridPane) throws IOException {
         txtQuestion= new Text();
+        txtScore= new Text();
         txtCode= new TextArea();
         txtAns= new TextField();
         btnSubmit= new Button("Submit");
 
+        txtQuestion.setStyle("-fx-font: 22 arial;");
+        txtCode.setStyle("-fx-font: 22 arial;");
+        txtQuestion.setFill(Color.WHITE);
+
+        txtScore.setStyle("-fx-font: 16 arial;");
+        txtScore.setFill(Color.WHITE);
         txtCode.setEditable(false);
 
-        gridPane.setMinSize(800, 720);
+        gridPane.setMinSize(1200, 900);
         gridPane.setPadding(new Insets(10, 10, 10, 10));
 
         //Setting the vertical and horizontal gaps between the columns
-        gridPane.setVgap(30);
-        gridPane.setHgap(30);
+        gridPane.setVgap(50);
+        gridPane.setHgap(50);
 
         //Setting the Grid alignment
         gridPane.setAlignment(Pos.CENTER);
@@ -49,16 +57,12 @@ public class Predict extends Application {
         gridPane.add(txtCode, 0, 1);
         gridPane.add(txtAns, 0, 2);
         gridPane.add(btnSubmit, 0, 3);
+        gridPane.add(txtScore, 0, 4);
 
-        gridPane.setStyle("-fx-background-color: BEIGE;");
-        //gridPane.setStyle("-fx-background-color: #008080;");
+        //gridPane.setStyle("-fx-background-color: BEIGE;");
+        gridPane.setStyle("-fx-background-color: #005682;");
     }
 
-    void ProgressUpdate() throws IOException {
-        FileWriter fout= new FileWriter("progress.txt");
-        fout.write(pos+"");
-        fout.flush();
-    }
 
     int getPos() throws FileNotFoundException {
         FileReader fin= new FileReader("progress.txt");
@@ -73,111 +77,150 @@ public class Predict extends Application {
             pos= pos*10+ (str.charAt(i)-'0');
         }
 
+        if(sc.hasNext())
+            str= sc.nextLine();
+
+        int cor= 0;
+        for(int i=0;i<str.length();i++){
+            cor= cor*10+ (str.charAt(i)-'0');
+        }
+
+        corr= cor;
         return pos;
     }
 
-    void addQuesntion() throws IOException {
-        FileReader fin= new FileReader("codes.txt");
-        Scanner sc= new Scanner(fin);
 
-        String q="", code="",ans="";
-        int fl=0;
-        while(sc.hasNext()){
-            String str= sc.nextLine();
-
-            if(str.equals("-->")){
-                Codes.add(new Code(ans,q,code));
-                q=""; code=""; ans="";
-                fl=0;
-                continue;
-            }
-
-            if(fl==0){
-                ans+=str;
-                fl=1;
-            }
-            else if(fl==1) {
-                q+= str;
-                fl=2;
-            }
-            else
-                code+=str+"\n";
-        }
-        fin.close();
-        sc.close();
-    }
-
-    void setQuesion(int pos) {
-        Code code= Codes.get(pos);
-
-        txtQuestion.setText(code.getQ());
-        txtCode.setText(code.getCode());
-    }
-
-    boolean match(int pos){
-        Code code= Codes.get(pos);
-        String corrAns= code.getAns();
-        String ans= txtAns.getText().trim();
-
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("Confirmation Dialog");
-
-        if(ans.equals("")){
-            alert.setHeaderText("You can't leave the ans field empty");
-            alert.showAndWait();
-        }
-
-        if(ans.equals(corrAns)){
-            Paint value0 = Paint.valueOf("00FF00");
-            txtAns.setStyle("-fx-control-inner-background: #" + value0.toString().substring(2));
-            alert.setHeaderText("Correct Answer!\n"+ans);
-            alert.showAndWait();
-            Paint value1 = Paint.valueOf("FFFFFF");
-            txtAns.setStyle("-fx-control-inner-background: #" + value1.toString().substring(2));
-            return true;
-        }
-
-        else{
-            Paint value0 = Paint.valueOf("FF0000");
-            txtAns.setStyle("-fx-control-inner-background: #" + value0.toString().substring(2));
-            alert.setHeaderText("Wrong Answer! :(\nCorrect answer is: "+corrAns);
-            alert.showAndWait();
-            Paint value1 = Paint.valueOf("FFFFFF");
-            txtAns.setStyle("-fx-control-inner-background: #" + value1.toString().substring(2));
-            return false;
-        }
-    }
-
-    int pos= 0;
 
     @Override
     public void start(Stage stage) throws Exception {
         GridPane gridPane= new GridPane();
         init(gridPane);
+        classStage= stage;
 
         Scene scene = new Scene(gridPane);
         stage.setTitle("Predict The Output");
         stage.setScene(scene);
         stage.show();
 
-        addQuesntion();
-
         pos= getPos();
-        setQuesion(pos);
+        CodeArranger codeArranger= new CodeArranger();
+        codeArranger.addQuestion();
+        codeArranger.setQuestion(pos);
+        codeArranger.setScore(pos);
+
         btnSubmit.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                match(pos);
-                setQuesion(++pos);
-                txtAns.setText("");
+                if(pos>=Codes.size()){
+                    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                    alert.setHeaderText("You have attempted all the questions available!");
+                    alert.showAndWait();
+                    return;
+                }
+
                 try {
-                    ProgressUpdate();
+                    pos= codeArranger.Judge(pos);
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    System.out.println("failed");
                 }
             }
         });
 
     }
-}
 
+    private class CodeArranger extends Template{
+        @Override
+        void addQuestion() throws IOException {
+            FileReader fin= new FileReader("codes.txt");
+            Scanner sc= new Scanner(fin);
+
+            String q="", code="",ans="";
+            int fl=0;
+            while(sc.hasNext()){
+                String str= sc.nextLine();
+
+                if(str.equals("-->")){
+                    Codes.add(new Code(ans,q,code));
+                    q=""; code=""; ans="";
+                    fl=0;
+                    continue;
+                }
+
+                if(fl==0){
+                    ans+=str;
+                    fl=1;
+                }
+                else if(fl==1) {
+                    q+= str;
+                    fl=2;
+                }
+                else
+                    code+=str+"\n";
+            }
+            fin.close();
+            sc.close();
+        }
+
+        @Override
+        void setQuestion(int pos) {
+            if(pos>=Codes.size()){
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setHeaderText("You have attempted all the questions available!");
+                alert.showAndWait();
+                return;
+            }
+            Code code= Codes.get(pos);
+
+            txtQuestion.setText(code.getQ());
+            txtCode.setText(code.getCode());
+        }
+
+        @Override
+        void setScore(int pos) {
+            txtScore.setText("Attemped: "+pos+"\n\nCorrect: "+corr);
+        }
+
+        @Override
+        void match(int pos) {
+            Code code= Codes.get(pos);
+            String corrAns= code.getAns();
+            String ans= txtAns.getText().trim();
+
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirmation Dialog");
+
+            if(ans.equals("")){
+                alert.setHeaderText("You can't leave the ans field empty");
+                alert.showAndWait();
+            }
+
+            if(ans.equals(corrAns)){
+                corr++;
+                Paint value0 = Paint.valueOf("00FF00");
+                txtAns.setStyle("-fx-control-inner-background: #" + value0.toString().substring(2));
+                alert.setHeaderText("Correct Answer!\n"+ans);
+                alert.showAndWait();
+                Paint value1 = Paint.valueOf("FFFFFF");
+                txtAns.setStyle("-fx-control-inner-background: #" + value1.toString().substring(2));
+                txtAns.setText("");
+            }
+
+            else{
+                Paint value0 = Paint.valueOf("FF0000");
+                txtAns.setStyle("-fx-control-inner-background: #" + value0.toString().substring(2));
+                alert.setHeaderText("Wrong Answer! :(\nCorrect answer is: "+corrAns);
+                alert.showAndWait();
+                Paint value1 = Paint.valueOf("FFFFFF");
+                txtAns.setStyle("-fx-control-inner-background: #" + value1.toString().substring(2));
+                txtAns.setText("");
+            }
+        }
+
+        @Override
+        void progressUpdate(int pos) throws IOException {
+            FileWriter fout= new FileWriter("progress.txt");
+            fout.write(pos+"\n"+corr);
+            fout.flush();
+        }
+    }
+}
