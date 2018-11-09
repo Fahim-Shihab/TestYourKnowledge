5fe7defda30376cbc3e175e4f1638876b1e9afb8
Sakib1127 <30300839+Sakib1127@users.noreply.github.com>
2018-11-09 19:48:16 +0600

    Add files via upload

--- src/database/Add_new_subject.java
+++ src/database/Add_new_subject.java
@@ -1,9 +1,6 @@
 package database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Scanner;
 
 public class Add_new_subject extends Database_connector {
@@ -12,27 +9,34 @@ public class Add_new_subject extends Database_connector {
     public Add_new_subject(String subject_name) throws SQLException {
 
 
-        //Sakib's code after Shan's editing
-       /* this.subject_name = subject_name;
-
-        PreparedStatement dropTable = conn.prepareStatement(
-                String.format("DROP TABLE IF EXISTS %s", subject_name));
-        dropTable.execute();
-
-        PreparedStatement createTable = conn.prepareStatement("CREATE TABLE "+subject_name+"(id int NOT NULL AUTO_INCREMENT, Question varchar(5000), Answer varchar(5000), Option1 varchar(5000),Option2 varchar(5000),Option3 varchar(5000), PRIMARY KEY(id))") ;
-        createTable.executeUpdate();*/
-
-       //Sakib's code after edited by Sakib adding some new lines
        this.subject_name = subject_name;
         //Connection conn = null;
         //conn = DriverManager.getConnection(CONNECTION_STRING,USERNAME,PASSWORD);
         PreparedStatement createTable1 = conn.prepareStatement("CREATE TABLE IF NOT EXISTS "+subject_name+"(id int NOT NULL AUTO_INCREMENT, Question varchar(5000), Answer varchar(5000), Option1 varchar(5000),Option2 varchar(5000),Option3 varchar(5000), PRIMARY KEY(id))") ;
        // PreparedStatement createTable1 = conn.prepareStatement("CREATE TABLE IF NOT EXISTS "+subject_name+"(id int NOT NULL AUTO_INCREMENT, Name varchar(500), Teacher_id varchar(500), Department varchar(500), User_name varchar(500), Password varchar(500),Subject varchar(500), PRIMARY KEY(id))") ;
         createTable1.executeUpdate();
-       // String insert ="INSERT INTO subject_table (Subjects) VALUES ('"+subject_name+"')";
-        //PreparedStatement insert_subject =  conn.prepareStatement(insert);
-        //insert_subject.executeUpdate();
+        Statement statement = conn.createStatement();
 
+
+        ResultSet rs = statement.executeQuery("SELECT Subjects FROM subject_table");
+        int i = 0;
+        int flag = 0;
+        while (rs.next())
+        {
+            System.out.println(rs.getString(1));
+            if(subject_name.equals(rs.getString(1)))
+            flag = 1;
+
+
+            //System.out.println(rs.getString(1));
+            //  System.out.println(this.subject_array[i-1]);
+        }
+
+        if(flag == 0) {
+            String insert = "INSERT INTO subject_table (Subjects) VALUES ('" + subject_name + "')";
+            PreparedStatement insert_subject = conn.prepareStatement(insert);
+            insert_subject.executeUpdate();
+        }
     }
 
 }
